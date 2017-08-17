faff23f81ea2e9fefa0c1a21eb2897cf813cec10
hysys32 <hysys32@qq.com>
2017-08-17 10:01:40 +0800

    selector for xpath & css

--- ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -5,7 +5,10 @@ import scrapy
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/112170/']
 
     def parse(self, response):
+        # re_selector=response.xpath('//*[@id="post-112170"]/div[1]/h1')
+        re_selector=response.xpath('//div[@class="entry-header"]/h1/text()')
         pass
+
